cc82eb7af7001fb01878357b6773e4188e41efab
SEULGI YOON <sgyoon.info@gmail.com>
2020-03-23 14:33:58 +0900

    Add react-virtualized

--- 10-todo-app/src/App.js
+++ 10-todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from 'components/TodoTemplate';
 import TodoInsert from 'components/TodoInsert';
 import TodoList from 'components/TodoList';
@@ -15,45 +15,46 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(createBulkTodos);
+  // useReducer의 두번째 인자로 초기값을 넣어주는데, 그를 undefined로 주고 함수를 세번째 인자로 넣으면 첫 렌더시에만 함수가 호출된다
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 할일 목록에 붙을 id. 화면에 랜더되거나 이 값으로 인해 컴포넌트가 리랜더되어야 할 필요가 없으므로
   // useRef를 이용한다 (211p)
   const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    text => {
-      const newTodo = {
-        id: nextId.current,
-        text: text,
-        checked: false,
-      };
-      // useState의 함수형 업데이트. 바로 새 값을 넣지 않고, 어떻게 업데이트할지를 정해준다.
-      // 이렇게 하면 52번줄에 todos를 넣지 않아도 되고, 따라서 todos가 바뀔 때마다 함수가 바뀌는 일이 사라진다.
-      setTodos(todos => todos.concat(newTodo));
-      nextId.current = nextId.current + 1;
-    },
-    [],
-  );
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text: text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current = nextId.current + 1;
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
-    },
-    [],
-  );
+  const onRemove = useCallback(id => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(todos =>
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [],
-  );
+  const onToggle = useCallback(id => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
--- 10-todo-app/src/components/TodoList.js
+++ 10-todo-app/src/components/TodoList.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 function TodoList({ todos, onRemove, onToggle }) {
-  return (
-    <div>
-      {todos.map(todo => (
+  // List 컴포넌트에서 각 TodoListItem을 랜더할 때 사용할 함수
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
-          key={todo.id}
+          key={key}
           todo={todo}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [todos, onRemove, onToggle],
+  );
+
+  return (
+    <List
+      className="TodoList"
+      width={512} // 리스트 전체 가로 크기
+      height={513} // 리스트 전체 높이
+      rowCount={todos.length} // 전체 항목 개수
+      rowHeight={57} // 아이템 개별 항목의 높이
+      rowRenderer={rowRenderer} // 아이템 개별 항목을 랜더할 때 사용하는 함수
+      list={todos} // 랜더할 배열
+      style={{ outline: 'none'}} // List에 기본 적용되는 outline 스타일 제거
+    />
   );
 }
 
-export default TodoList;
+export default React.memo(TodoList);
