be37425a86058627f610ba37be1206c479daee2e
SEULGI YOON <sgyoon.info@gmail.com>
2020-03-25 14:12:40 +0900

    Add axios

--- 14-news-viewer/src/App.js
+++ 14-news-viewer/src/App.js
@@ -1,86 +1,34 @@
-import React from "react";
-
-// function increase(number, count, callback) {
-//   setTimeout(() => {
-//     const result = number + 10;
-//     const newCount = count + 1;
-//     if (callback) {
-//       callback(newCount, result);
-//     }
-//   }, 1000);
-// }
-
-// // 콜백 지옥 만들기
-// console.log("시작");
-// increase(0, 0, (count, result) => {
-//   console.log(`${count}번째, ${result}`); // 1번째, 10
-//   increase(result, count, (count, result) => {
-//     console.log(`${count}번째, ${result}`); // 2번째, 20
-//     increase(result, count, (count, result) => {
-//       console.log(`${count}번째, ${result}`); // ...
-//       increase(result, count, (count, result) => {
-//         console.log(`${count}번째, ${result}`);
-//         increase(result, count, (count, result) => {
-//           console.log(`${count}번째, ${result}`);
-//           console.log("끝");
-//         });
-//       });
-//     });
-//   });
-// });
-
-// // Promise 이용
-// function increase(count, number) {
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const newNumber = number + 10;
-//       const newCount = count + 1;
-//       if (newNumber > 30) {
-//         const error = new Error('여기까지입니다');
-//         return reject(error);
-//       }
-//       resolve({count: newCount, number: newNumber});
-//     }, 1000);
-//   });
-//   return promise;
-// }
-
-// increase(0, 0)
-// .then((result) => {
-//   console.log(`${result.count}번째, ${result.number}`);
-//   return increase(result.count, result.number);
-// })
-// .then((result) => {
-//   console.log(`${result.count}번째, ${result.number}`);
-//   return increase(result.count, result.number);
-// })
-// .then((result) => {
-//   console.log(`${result.count}번째, ${result.number}`);
-//   return increase(result.count, result.number);
-// })
-// .catch((error) => console.error(error));
-
-// // async/await
-// async function tasks() {
-//   try {
-//     let result = await increase(0, 0);
-//     console.log(`${result.count}번째, ${result.number}`);
-//     result = await increase(result.count, result.number);
-//     console.log(`${result.count}번째, ${result.number}`);
-//     result = await increase(result.count, result.number);
-//     console.log(`${result.count}번째, ${result.number}`);
-//     result = await increase(result.count, result.number);
-//     console.log(`${result.count}번째, ${result.number}`);
-//   }
-//   catch(error) {
-//     console.error(error);
-//   }
-// }
-
-// tasks();
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
 
 function App() {
-  return <div></div>;
+  const [data, setData] = useState('Hello');
+  // const getData = useCallback(() => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then(result => setData(result.data))
+  //     .catch(error => console.error(error));
+  // }, []);
+
+  const getData = async () => {
+    try {
+      const result = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setData(result.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={getData}>불러오기</button>
+      <div>
+        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
+      </div>
+    </div>
+  );
 }
 
 export default App;
