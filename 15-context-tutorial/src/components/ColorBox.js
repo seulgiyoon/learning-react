52fa1c68276d48bbeae7962c90cd059056c97dc8
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-02 11:28:33 +0900

    Use hook to lookup context

--- 15-context-tutorial/src/components/ColorBox.js
+++ 15-context-tutorial/src/components/ColorBox.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 function ColorBox() {
+  // useContext로 바로 컨텍스트의 값을 조회한다
+  const { state } = useContext(ColorContext);
   return (
-    // Consumer를 통해서 Provider에 접근할 수 있다.
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-        <div 
-          style={{ 
-            width: '50px', 
-            height: '50px', 
-            background: state.color 
-          }} 
-        />
-        <div 
+    <>
+      <div 
+        style={{ 
+          width: '50px', 
+          height: '50px', 
+          background: state.color 
+        }} 
+      />
+      <div 
         style={{ 
           width: '80px', 
           height: '80px', 
           background: state.subcolor 
         }} 
       />
-      </>
-      )}
-    </ColorConsumer>
+    </>
   );
 };
 
--- 15-context-tutorial/src/components/SelectColors.js
+++ 15-context-tutorial/src/components/SelectColors.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, { Component } from 'react';
+import ColorContext from '../contexts/color';
 
 const colors = ['tomato', 'orange', 'gold', 'green', 'blue'];
 
-function SelectColors() {
-  return (
-    <div style={{ marginBottom: '30px' }}>
-      <h2>색상을 선택하세요.</h2>
-      {/* Consumer를 통해서 Provider에 접근할 수 있다. */}
-      <ColorConsumer>
-      {({actions}) => (
-      <div style={{display: 'flex'}}>
-        {colors.map((color, index) => (
-          <div 
-            key={index} 
-            style={{
-              width: '30px', 
-              height: '30px', 
-              background: color, 
-              cursor: 'pointer'
-            }} 
-            onClick={() => actions.setColor(color)}
-            onContextMenu={(e) => {
-              e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것 무시
-              actions.setSubcolor(color);
-            }}
-          />
-        ))}
+class SelectColors extends Component {
+
+  static contextType = ColorContext;
+
+  render() {
+    return (
+      <div style={{ marginBottom: '30px' }}>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{display: 'flex'}}>
+          {colors.map((color, index) => (
+            <div 
+              key={index} 
+              style={{
+                width: '30px', 
+                height: '30px', 
+                background: color, 
+                cursor: 'pointer'
+              }} 
+              onClick={() => this.context.actions.setColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것 무시
+                this.context.actions.setSubcolor(color);
+              }}
+            />
+          ))}
+        </div>
       </div>
-      )}
-      </ColorConsumer>
-    </div>
-  )
+    )
+  }
 }
 
-export default SelectColors
+export default SelectColors;
