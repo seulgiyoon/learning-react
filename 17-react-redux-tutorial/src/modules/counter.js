c01fa0ba75005a5640d9137413722d201db781df
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-07 10:44:17 +0900

    Use redux-actions and immer

--- 17-react-redux-tutorial/src/modules/counter.js
+++ 17-react-redux-tutorial/src/modules/counter.js
@@ -1,32 +1,27 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의. 문자열의 내용은 이름 충돌을 방지하기 위해서 '모듈 이름/액션 이름' 형태로 작성한다.
 const INCREASE = 'counter/INCRESE';
 const DECREASE = 'counter/DECREASE';
 
 // export 키워드로 다른 파일에서 불러올 수 있도록 액션 생성 함수를 작성
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 state
 const initialState = {
   number: 0,
-}
+};
 
 // 리듀서 함수
-const counter = (state = initialState, action) => {
-  switch(action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1
-      }
-    case DECREASE:
-      return {
-        number: state.number - 1
-      }
-    default:
-      return state
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
-// 현재 파일에서 내보낸 함수들을 다른 파일에서 import한다면 
+// 현재 파일에서 내보낸 함수들을 다른 파일에서 import한다면
 // import counter, { increase, decrease } from './counter'가 된다
 export default counter;
--- 17-react-redux-tutorial/src/modules/todos.js
+++ 17-react-redux-tutorial/src/modules/todos.js
@@ -1,3 +1,6 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
 // 액션 타입 정의
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값 변경
 const INSERT = 'todos/INSERT'; // 새로운 todo 등록
@@ -5,21 +8,15 @@ const TOGGLE = 'todos/TOGGLE'; // todo의 완료/미완료 상태 변경
 const REMOVE = 'todos/REMOVE'; // todo 삭제
 
 // 액션 생성 함수 -> 들어온 데이터를 가능하면 알맞은 형태로 처리
-export const changeInput = (input) => ({ type: CHANGE_INPUT, input });
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
 let id = 3;
-export const insert = (text) => ({
-  type: INSERT,
-  todo: {
-    id: id + 1,
-    text: text,
-    done: false,
-  },
-});
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-});
-export const remove = (id) => ({ type: REMOVE, id });
+export const insert = createAction(INSERT, (text) => ({
+  id: id + 1,
+  text,
+  done: false,
+}));
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 // 초기값
 const initialState = {
@@ -31,34 +28,24 @@ const initialState = {
 };
 
 // 리듀서
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo),
-      };
-    case TOGGLE:
-      const newTodos = state.todos.map((todo) =>
-        todo.id === action.id ? { ...todo, done: !todo.done } : todo,
-      );
-      return {
-        ...state,
-        todos: newTodos,
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
-    default:
-      return state;
-  }
-};
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => produce(draft => draft.input = input),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => 
+      produce(draft => {
+        const targetTodo = draft.find(todo => todo.id === id);
+        targetTodo.done = !targetTodo.done
+      }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState,
+);
 
 export default todos;
