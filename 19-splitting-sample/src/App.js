3ed814f86c034a21a8229c2d01ad60000f152a0d
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-17 17:56:36 +0900

    Use React.lazy and Suspense

--- 19-splitting-sample/src/App.js
+++ 19-splitting-sample/src/App.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState, Suspense } from 'react'
 
-class App extends Component {
-  state = {
-    SplitMe: null
-  };
-
-  handleClick = async() => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default
-    });
-  }
-
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div>
-        <button onClick={this.handleClick}>Click</button>
-        {SplitMe && <SplitMe />}
-      </div>
-    )
-  }
+function App() {
+  const [visible, setVisible] = useState(false);
+  const SplitMe = React.lazy(() => import('./SplitMe'));
+  
+  return (
+    <div>
+      <button onClick={() => setVisible(true)}>Click</button>
+      <Suspense fallback={<div>로딩중...</div>}>
+        {visible && <SplitMe />}
+      </Suspense>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
+// class 형태
+// import React, { Component, Suspense } from 'react'
+
+// export default class App extends Component {
+//   state = {
+//     SplitMe: null
+//   }
+
+//   handleClick = () => {
+//     const contents = React.lazy(() => import('./SplitMe'));
+//     this.setState({
+//       SplitMe: contents
+//     })
+//   } 
+
+//   render() {
+//     const { SplitMe } = this.state
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>Click</button>
+//         <Suspense fallback={<div>로딩중...</div>}>
+//           {SplitMe && <SplitMe />}
+//         </Suspense>
+//       </div>
+//     )
+//   }
+// }
