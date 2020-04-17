66e0d2f665cb90fe6c138b0cec60cd9592f035e8
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-17 18:10:53 +0900

    Use Loadable components

--- 19-splitting-sample/src/App.js
+++ 19-splitting-sample/src/App.js
@@ -1,45 +1,31 @@
-import React, { useState, Suspense } from 'react'
+import React, { useState } from 'react';
+import loadable from '@loadable/component';
+
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>로딩 중...</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
-  const SplitMe = React.lazy(() => import('./SplitMe'));
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    // 버튼에 마우스를 올린 시점부터 컴포넌트를 미리 불러온다
+    SplitMe.preload();
+  };
   
   return (
     <div>
-      <button onClick={() => setVisible(true)}>Click</button>
-      <Suspense fallback={<div>로딩중...</div>}>
-        {visible && <SplitMe />}
-      </Suspense>
+      <button 
+        onClick={onClick}
+        onMouseOver={onMouseOver}
+      >
+        Click
+      </button>
+      {visible && <SplitMe />}
     </div>
-  )
+  );
 }
 
-export default App
-
-// class 형태
-// import React, { Component, Suspense } from 'react'
-
-// export default class App extends Component {
-//   state = {
-//     SplitMe: null
-//   }
-
-//   handleClick = () => {
-//     const contents = React.lazy(() => import('./SplitMe'));
-//     this.setState({
-//       SplitMe: contents
-//     })
-//   } 
-
-//   render() {
-//     const { SplitMe } = this.state
-//     return (
-//       <div>
-//         <button onClick={this.handleClick}>Click</button>
-//         <Suspense fallback={<div>로딩중...</div>}>
-//           {SplitMe && <SplitMe />}
-//         </Suspense>
-//       </div>
-//     )
-//   }
-// }
+export default App;
