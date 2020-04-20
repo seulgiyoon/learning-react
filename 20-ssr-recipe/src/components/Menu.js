16cf8dc194a01ece6f84d79a15c6235c5642bc57
SEULGI YOON <sgyoon.info@gmail.com>
2020-04-20 10:59:35 +0900

    Commit before eject

---
+++ 20-ssr-recipe/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Menu() {
+  return (
+    <ul>
+      <li><Link to="/red">Red</Link></li>
+      <li><Link to="/blue">Blue</Link></li>
+    </ul>
+  )
+}
+
+export default Menu
